2e8025756c304eef8f0f969a2e69c04047599084
DanutzB <danielbabii@yahoo.com>
2018-11-11 15:02:45 +0200

    model proiect

---
+++ src/main/java/Car.java
@@ -0,0 +1,28 @@
+public class Car {
+
+    private String category;
+    private String combustible;
+    private String transmission;
+    private Double engineCapacity;
+    private Double tankCapacity;
+    private Double fuelConsumption;
+    private Double emissions;
+    private String color;
+
+    public Car(String category, String combustible, String transmission, Double engineCapacity,
+               Double tankCapacity, Double fuelConsumption, Double emissions, String color) {
+        this.category = category;
+        this.combustible = combustible;
+        this.transmission = transmission;
+        this.engineCapacity = engineCapacity;
+        this.tankCapacity = tankCapacity;
+        this.fuelConsumption = fuelConsumption;
+        this.emissions = emissions;
+        this.color = color;
+    }
+
+
+
+
+
+}
