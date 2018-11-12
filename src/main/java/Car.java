c04a2a3fde3f92e54b623545b3767086de8721b5
Apetri George <apetrigeorge@yahoo.com>
2018-11-12 22:45:25 +0200

    configurator...nu imi place cum arata

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,13 +1,25 @@
 public class Car {
 
+//    category: suv, hatchback, sedan
     private String category;
+//    combustible: diesel, benzine
     private String combustible;
+//    transmission: manual, automatic
     private String transmission;
+//    engineCapacity: 1.4, 1.6 , 2.0
     private Double engineCapacity;
+//    tankCapacity: 50l, 60l, 70l
     private Double tankCapacity;
+//    fuelConsumption: 6%, 8%, 10%
     private Double fuelConsumption;
+//    emissions: 90g/km, 100g/km, 110g/km
     private Double emissions;
+//    color: red, blue, black
     private String color;
+
+    public Car(){
+
+    }
 
     public Car(String category, String combustible, String transmission, Double engineCapacity,
                Double tankCapacity, Double fuelConsumption, Double emissions, String color) {
@@ -21,8 +33,67 @@ public class Car {
         this.color = color;
     }
 
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
 
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
 
+    public void setEngineCapacity(Double engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
 
+    public void setTankCapacity(Double tankCapacity) {
+        this.tankCapacity = tankCapacity;
+    }
+
+    public void setFuelConsumption(Double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setEmissions(Double emissions) {
+        this.emissions = emissions;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public Double getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public Double getTankCapacity() {
+        return tankCapacity;
+    }
+
+    public Double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public Double getEmissions() {
+        return emissions;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,7 +1,47 @@
+import java.util.Scanner;
+
 public class Order {
+
+    private Car car;
+    private Client client;
 
     public void orderCar(){
 
+
     }
+
+    public void startConfigurator(){
+        Car car = new Car();
+        System.out.println("Please enter customer name: ");
+        Scanner scanner = new Scanner(System.in);
+        Client client = new Client(scanner.nextLine());
+
+        System.out.println("Please enter car category: suv, hatchback, sedan");
+        car.setCategory(scanner.nextLine());
+
+        System.out.println("Please enter car combustible: diesel, benzine");
+        car.setCombustible(scanner.nextLine());
+
+        System.out.println("Please enter car transmission: manual, automatic");
+        car.setTransmission(scanner.nextLine());
+
+        System.out.println("Please enter car engineCapacity: 1.4, 1.6 , 2.0");
+        car.setEngineCapacity(scanner.nextDouble());
+
+        System.out.println("Please enter car tankCapacity: 50l, 60l, 70l");
+        car.setTankCapacity(scanner.nextDouble());
+
+        System.out.println("Please enter car fuelConsumption: 6%, 8%, 10%");
+        car.setFuelConsumption(scanner.nextDouble());
+
+        System.out.println("Please enter car emissions: 90g/km, 100g/km, 110g/km");
+        car.setEmissions(scanner.nextDouble());
+
+        System.out.println("Please enter car color: red, blue, black");
+        car.setColor(scanner.nextLine());
+
+        System.out.println("Configuration: " + car.getCategory() + " " + car.getColor() + "restul...");
+    }
+
 
 }
