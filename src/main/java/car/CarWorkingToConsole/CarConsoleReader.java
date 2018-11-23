5beb76d4c6e060589783bb478a2c17adfe642450
icraciunescu <44140542+icraciunescu@users.noreply.github.com>
2018-11-23 18:03:18 +0200

    Add files via upload

--- src/main/java/car/CarWorkingToConsole/CarConsoleReader.java
+++ src/main/java/car/CarWorkingToConsole/CarConsoleReader.java
@@ -11,45 +11,69 @@ public class CarConsoleReader {
         Car car = new Car();
         Scanner scanner = new Scanner( System.in );
 
-        System.out.println("car configurator");
+        System.out.println( "car configurator" );
+        System.out.println( "select car option" );
+        System.out.println();
 
-        System.out.println("category");
-        car.setCategory( scanner.nextLine() );
+        int optionCategory;
+        System.out.println( "category" );
+        System.out.println();
+        System.out.println( "1   suv" );
+        System.out.println( "2   sedan" );
+        optionCategory = scanner.nextInt();
+        if (optionCategory == 1) {
+            car.setCategory( "suv" );
+        }
+        if (optionCategory == 2) {
+            car.setCategory( "sedan" );
+        }
 
-        System.out.println("combustible");
-        car.setCombustible( scanner.nextLine() );
 
-        System.out.println("transmission");
-        car.setTransmission( scanner.nextLine() );
+        int optionCombustible;
+        System.out.println( "combustible" );
+        System.out.println();
+        System.out.println( "1   benzine" );
+        System.out.println( "2   diesel" );
+        optionCombustible = scanner.nextInt();
+        if (optionCombustible == 1) {
+            car.setCombustible( "benzine" );
+        }
+        if (optionCombustible == 2) {
+            car.setCombustible( "diesel" );
+        }
 
-        System.out.println("engine capacity");
+
+        System.out.println( "transmission" );
+        car.setTransmission( scanner.next() );
+
+        System.out.println( "engine capacity" );
         car.setEngineCapacity( scanner.nextInt() );
 
-        System.out.println("tank capacity");
+        System.out.println( "tank capacity" );
 //        todo calculateTankCapacity
-        System.out.println("calculateTankCapacity");
+        System.out.println( "calculateTankCapacity" );
 
-        System.out.println("fuel consumption");
+        System.out.println( "fuel consumption" );
 //        todo calculateFuelConsumption
-        System.out.println("calculateFuelConsumption");
+        System.out.println( "calculateFuelConsumption" );
 
-        System.out.println("emission");
+        System.out.println( "emission" );
 //        todo calculateEmission
-        System.out.println("calculateEmission");
+        System.out.println( "calculateEmission" );
 
-        System.out.println("color");
+        System.out.println( "color" );
         car.setColor( scanner.next() );
 
         return car;
 
     }
 
-    public Car editCategory(Car car){
+    public Car editCategory(Car car) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("edit car data");
-        System.out.println("edit category");
-        car.setCategory(scanner.nextLine());
+        Scanner scanner = new Scanner( System.in );
+        System.out.println( "edit car data" );
+        System.out.println( "edit category" );
+        car.setCategory( scanner.nextLine() );
 
         return car;
     }
